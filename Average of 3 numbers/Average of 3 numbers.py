3708db46f6d4db866f92ea4ed189aa6d02f23e19
Salman Faris <119687229+Salman-Faris3090@users.noreply.github.com>
2023-01-11 11:25:07 +0530

    Add files via upload

---
+++ Average of 3 numbers/Average of 3 numbers.py
@@ -0,0 +1,7 @@
+# Average of 3 numbers
+
+a=int(input("Enter First Number="))
+b=int(input("Enter Second number="))
+c=int(input("Enter Third number="))
+avg=(a+b+c)/3
+print("Average of Three Numbers=",avg)
