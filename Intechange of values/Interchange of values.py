0e1478e89f7e132e69a0c3ac44c10c9fd59614e3
Salman Faris <119687229+Salman-Faris3090@users.noreply.github.com>
2023-01-11 23:28:18 +0530

    Delete Interchange of values.py

--- Intechange of values/Interchange of values.py
+++
@@ -1,9 +0,0 @@
-# interchanging two numbers
-
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-print("The two numbers are:",a,",",b)
-c=a
-a=b
-b=c
-print("The numbers after interchanging=",a,",",b)
