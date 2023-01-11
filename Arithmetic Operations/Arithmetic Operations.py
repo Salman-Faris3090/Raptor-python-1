02aff6ea6bf339b34fda56305501bfcc3c57083e
Salman Faris <119687229+Salman-Faris3090@users.noreply.github.com>
2023-01-11 23:22:35 +0530

    Add files via upload

---
+++ Arithmetic Operations/Arithmetic Operations.py
@@ -0,0 +1,18 @@
+# arithmetic operations
+
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+sum=a+b
+print("The sum of numbers is:",sum)
+
+diff=a-b
+print("The difference is:",diff)
+
+pro=a*b
+print("The product of two numbers is:",pro)
+
+quo=a//b
+print("The quotient of two numbers is:",quo)
+
+rem=a%b
+print("The reminder of two numbers is:",rem)
