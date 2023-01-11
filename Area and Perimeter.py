e8ada08e2a1a0549e70e026d2dc53da7a26646a8
Salman Faris <119687229+Salman-Faris3090@users.noreply.github.com>
2023-01-11 10:22:53 +0530

    Delete Area and Perimeter.py

--- Area and Perimeter.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter First side="))
-b=int(input("Enter Second side="))
-c=int(input("Enter Third side="))
-per=a+b+c
-s=per/2
-import math
-area=math.sqrt(s*(s-a)*(s-b)*(s-c))
-print("Area of a triangle=",area)
-print("Perimeter of a triangle=",per)
